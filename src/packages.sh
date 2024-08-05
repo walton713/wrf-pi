32ee0cd3331c44fa3f284d6241488f95ecd45f36
Ben Walton <walton713@gmail.com>
2024-08-05 12:28:58 -0500

    separate steps

--- src/packages.sh
+++ src/packages.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
-install_packages() {
+update_and_install_packages() {
   print_block_message "Updating system and installing required packages"
+  update
+  upgrade
+  install
+}
 
+update() {
   print_update_message "Updating system..."
   run_command "sudo apt update"
+}
 
+upgrade() {
   print_update_message "Upgrading packages..."
   run_command "sudo apt upgrade -y"
+}
 
+install() {
   print_update_message "Installing required packages..."
   run_command "sudo apt-get install gfortran libcurl4-gnutls-dev libxml2-dev m4 cmake libjpeg-dev -y"
 }
