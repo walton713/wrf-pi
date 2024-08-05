4a679c5cb229e40a8437125b48bb18d12a75a412
Ben Walton <walton713@gmail.com>
2024-08-05 10:08:15 -0500

    add -y parameter to upgrade

--- src/packages.sh
+++ src/packages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 install_packages() {
-  sudo apt update && sudo apt upgrade
+  sudo apt update && sudo apt upgrade -y
   sudo apt-get install gfortran libcurl4-gnutls-dev libxml2-dev m4 cmake libjpeg-dev -y
 }
