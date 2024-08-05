f8ab8ca72499e70c72748e9d2f86e5365c003fb5
Ben Walton <walton713@gmail.com>
2024-08-05 11:28:26 -0500

    add verbose option

--- src/packages.sh
+++ src/packages.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 install_packages() {
-  sudo apt update && sudo apt upgrade -y
-  sudo apt-get install gfortran libcurl4-gnutls-dev libxml2-dev m4 cmake libjpeg-dev -y
+  print_block_message "Updating system and installing required packages"
+
+  print_update_message "Updating system..."
+  run_command "sudo apt update"
+
+  print_update_message "Upgrading packages..."
+  run_command "sudo apt upgrade -y"
+
+  print_update_message "Installing required packages..."
+  run_command "sudo apt-get install gfortran libcurl4-gnutls-dev libxml2-dev m4 cmake libjpeg-dev -y"
 }
--- src/utils.sh
+++ src/utils.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+RED=$'\e[0;31m'
 GREEN=$'\e[0;32m'
 NC=$'\e[0m'
 
-print_message() {
+print_block_message() {
   echo ""
   echo ""
+  echo -e "${GREEN}####################################################################################################${NC}"
   echo -e "${GREEN}####################################################################################################${NC}"
   echo -e "${GREEN}#${NC}"
   echo -e "${GREEN}#${NC}"
@@ -13,6 +15,29 @@ print_message() {
   echo -e "${GREEN}#${NC}"
   echo -e "${GREEN}#${NC}"
   echo -e "${GREEN}####################################################################################################${NC}"
+  echo -e "${GREEN}####################################################################################################${NC}"
   echo ""
   echo ""
+}
+
+print_update_message() {
+  echo -e "${GREEN}$1${NC}"
+}
+
+print_error_message() {
+  echo -e "${RED}$1${NC}"
+}
+
+run_command() {
+  if [[ $VERBOSE = true ]]
+  then
+    { ERR=$(${1} 2>&1 >&3 3>&-); } 3>&1
+  else
+    ERR=$(${1} 1>/dev/null 2>&1)
+  fi
+
+  if [[ $? -ne 0 ]]
+  then
+    print_error_message "Error while running command '${1}': ${ERR}"
+  fi
 }
